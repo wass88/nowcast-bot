5a1dcbaedc18d8c67bc4d982b4e77f53ebcab5c7
wass80 <wasss80@gmail.com>
2023-07-26 16:06:11 +0900

    skip no rain

--- gif.go
+++ gif.go
@@ -14,11 +14,15 @@ func (c NowcastComposer) ComposeGif() gif.GIF {
 		Delay:     []int{},
 		LoopCount: 0,
 	}
-	for _, img := range c.Annotated.Nowcast {
+	for i, img := range c.Annotated.Nowcast {
 		q := median.Quantizer(256)
 		paletted := q.Paletted(img.Image)
 		res.Image = append(res.Image, paletted)
-		res.Delay = append(res.Delay, delay)
+		if c.Rain.Rains[i].RainFall > 0 {
+			res.Delay = append(res.Delay, delay)
+		} else {
+			res.Delay = append(res.Delay, delay/5)
+		}
 	}
 	return res
 }
